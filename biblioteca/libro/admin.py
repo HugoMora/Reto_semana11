9073a8fe4ac9609e0dec58c29103344332af96ca
Jesus Roncal <jesusroncal94@gmail.com>
2019-10-26 13:41:03 -0500

    prueba de tablas

---
+++ biblioteca/libro/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Libro, Persona, Categoria, Alquiler, Tarifa, AlquilerDetalle, LibroCategoria
+
+# Register your models here.
+admin.site.register(Libro)
+admin.site.register(Persona)
+admin.site.register(Categoria)
+admin.site.register(Alquiler)
+admin.site.register(AlquilerDetalle)
+admin.site.register(Tarifa)
+admin.site.register(LibroCategoria)
---
+++ biblioteca/libro/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    #autor = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+class Alquiler(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.TextField()
+    fecha = models.DateField(auto_now_add=True)
+    persona = models.ForeignKey('Persona', on_delete=models.CASCADE)
+
+class Tarifa(models.Model):
+    id = models.AutoField(primary_key=True)
+    descripcion = models.TextField()
+    inicio = models.DateField()
+    fin = models.DateField()
+    precio_por_dia = models.IntegerField()
+
+class Persona(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    apellido_paterno = models.CharField(max_length=50)
+    apellido_materno = models.CharField(max_length=50)
+    dni = models.IntegerField()
+
+
+class AlquilerDetalle(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha_alquiler = models.DateField(auto_now_add=True)
+    cantidad_dias = models.IntegerField()
+    id_libro = models.ForeignKey('Libro', on_delete=models.CASCADE)
+    id_alquiler = models.ForeignKey('Alquiler', on_delete=models.CASCADE)
+    id_tarifa = models.ForeignKey('Tarifa', on_delete=models.CASCADE)
+
+class LibroCategoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    id_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
