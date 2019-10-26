07c19393dbaef6daf72b8bcc81ca420b2fa443bb
Jesus Roncal <jesusroncal94@gmail.com>
2019-10-26 13:59:37 -0500

    prueba4

--- biblioteca/libro/admin.py
+++ biblioteca/libro/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Libro, Persona, Categoria, Alquiler, Tarifa, AlquilerDetalle, LibroCategoria
+from .models import (Libro, Persona, Categoria,
+                    Alquiler, Tarifa, AlquilerDetalle,
+                    LibroCategoria)
 
 # Register your models here.
 admin.site.register(Libro)
